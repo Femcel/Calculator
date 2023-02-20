4f0684a0e8f0c66dbe1717319d52ea08d1bcbe98
Femcel <hugogfernandes0012@gmail.com>
2023-02-20 13:05:21 -0300

    Set up basic operations

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
-console.log("a");
+function Add(a, b) {
+  let result = a + b;
+  return result;
+} 
+
+function Subtract(a, b) {
+  let result = a - b;
+  return result;
+} 
+
+function Multiply(a, b) {
+  let result = a * b;
+  return result;
+} 
+function Divide(a, b) {
+  let result = a / b;
+  return result;
+} 
+
+function Operate(operation, a, b) {
+  switch (operation) {
+    case 'add':
+      return Add(a, b);
+
+    case 'subtract':
+      return Subtract(a, b);
+
+    case 'multiply':
+      return Multiply(a, b);
+
+    case 'divide':
+      return Divide(a, b);
+
+    default:
+      return console.log("Invalid Operation")
+  }
+}
+
+console.log(Operate('add', 5, 3));
+console.log(Operate('subtract', 5, 3));
+console.log(Operate('multiply', 5, 3));
+console.log(Operate('divide', 5, 3));
